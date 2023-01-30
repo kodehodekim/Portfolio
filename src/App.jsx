add4d730b1b91c9137c3cd4ea21b9e7ced1e531d
kodehodekim <kim.kodehode@gmail.com>
2023-01-30 22:12:54 +0100

    fixed grid

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@
 import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
 import NavigationBar from "./components/Nav";
+import "./index.css";
 
 function App() {
   return (
     <>
-      <NavigationBar />
-      <MainContent />
-      <Footer />
+      <div id="gridContainer">
+        <NavigationBar />
+        <MainContent />
+        <Footer />
+        {/* <div id="navComponent">
+          <NavigationBar />
+        </div>
+        <div id="mainComponent">
+          <MainContent />
+        </div>
+        <div id="footerComponent">
+          <Footer />
+        </div> */}
+      </div>
     </>
   );
 }
